18049add12e3cee6ad44172a5b6e91412a53483c
Dillon Huff <dillonhuff@gmail.com>
2015-02-09 01:57:25 -0600

    need to complete rosenbergPulse function

--- src/RosenbergPulse.hs
+++ src/RosenbergPulse.hs
@@ -1,7 +1,12 @@
 module RosenbergPulse() where
 
 rosenbergPulse :: Int -> Int -> Double -> Double -> [Double]
-rosenbergPulse f0 fs dutyCycle glottalOpenFrac = [0]
+rosenbergPulse f0 fs dutyCycle glottalOpenFrac =
+  let period = 1.0 / (fromIntegral f0)
+      pulseLength = floor $ period * (fromIntegral fs)
+      n2 = floor $ pulseLength * dutyCycle
+      n1 = floor $ glottalOpenFrac * n2 in
+  pulsePhases (floor n1) (floor n2) ((floor pulseLength) - (floor n2))
 
 pulsePhases :: Int ->
                Int ->
